f4a5797450ea84584b5323cbd7a6a3903b1dad61
VestaSapozhnikova <vesta.sapozhnikova@gmail.com>
2024-11-20 21:06:35 +0300

    Morphocode project completed

---
+++ script.js
@@ -0,0 +1,11 @@
+function subscribe() {
+  // Показать всплывающее окно
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  // Скрыть всплывающее окно
+  document.getElementById("popup").style.display = "none";
+}
+
+subscribe();
